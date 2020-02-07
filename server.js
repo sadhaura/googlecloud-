ed7f1b3f33b7a7373b9b849eb2071a17b8ed3e1e
rahuloneway <rahul@1wayit.com>
2020-02-07 11:18:03 +0530

    googlecloud3

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world in 2024\n');
+  res.send('Hello world in 2025\n');
 });
 
 app.listen(PORT, HOST);
